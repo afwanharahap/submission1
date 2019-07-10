6fb187db3fc78e4f61e822424e18209c0ab91db4
afwanharahap <51844102+afwanharahap@users.noreply.github.com>
2019-07-10 21:26:00 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
-// PHP Data Objects(PDO) Sample Code:
+<?php // PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:afwandb.database.windows.net,1433; Database = bimbingan_mhs", "afwan", "Harahap_puny4");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -7,9 +7,4 @@ catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "afwan@afwandb", "pwd" => "Harahap_puny4", "Database" => "bimbingan_mhs", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:afwandb.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-
+?>
