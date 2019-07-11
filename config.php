af867d398ea383dc7f8016a54e7f1c78b93d18fa
afwanharahap <51844102+afwanharahap@users.noreply.github.com>
2019-07-11 20:26:31 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,15 @@
-<?php // PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:afwandb.database.windows.net,1433; Database = bimbingan_mhs", "afwan", "Harahap_puny4");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+<?php
+
+$server = "afwandb.database.windows.net";
+$user = "afwan";
+$password = "Harahap_puny4";
+$nama_database = "bimbingan_mhs";
+
+$db = mysqli_connect($server, $user, $password, $nama_database);
+
+if( !$db ){
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
 }
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+
 ?>
+
