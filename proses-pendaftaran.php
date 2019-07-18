a0a4456b36b2885c75458b02f9ffc90bf8f61b6d
afwanharahap <51844102+afwanharahap@users.noreply.github.com>
2019-07-18 08:14:11 +0700

    Update proses-pendaftaran.php

--- proses-pendaftaran.php
+++ proses-pendaftaran.php
@@ -16,18 +16,5 @@ if(isset($_POST['daftar'])){
 	$sql = "INSERT INTO daftar_mhs (nama, alamat, jenis_kelamin, agama, judulta) VALUE ('$nama', '$alamat', '$jk', '$agama', '$judulta')";
 	$query = mysqli_query($db, $sql);
 	
-	// apakah query simpan berhasil?
-	if( $query ) {
-		// kalau berhasil alihkan ke halaman index.php dengan status=sukses
-		header('Location: index.php?status=sukses');
-	} else {
-		// kalau gagal alihkan ke halaman indek.ph dengan status=gagal
-		header('Location: index.php?status=gagal');
-	}
 	
-	
-} else {
-	die("Akses dilarang...");
-}
-
 ?>
