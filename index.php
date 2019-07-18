33c3caa0b5e7b399cfe486de9d53e7f9b72ebffb
afwanharahap <51844102+afwanharahap@users.noreply.github.com>
2019-07-18 08:11:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,11 +21,7 @@
 	
 	<?php if(isset($_GET['status'])): ?>
 	<p>
-		<?php
-			if($_GET['status'] == 'sukses'){
-				echo "Pendaftaran siswa baru berhasil!";
-			} 
-		?>
+		
 	</p>
 	<?php endif; ?>
 	
